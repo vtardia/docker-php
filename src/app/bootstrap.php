fd9788edddb69c396606f8d0a2bf5cdfbc7e4c28
Vito Tardia <vtardia@users.noreply.github.com>
2023-03-24 13:15:13 +0000

    Update Monolog and Slim

--- src/app/bootstrap.php
+++ src/app/bootstrap.php
@@ -1,27 +1,42 @@
 <?php
+
 require __DIR__ . '/vendor/autoload.php';
 
-use Slim\App;
+use Slim\Factory\AppFactory;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$app = new Slim\App;
+/**
+ * Slim 4.x does not ship with a container library.
+ * It supports all PSR-11 implementations such as PHP-DI
+ * To install PHP-DI `composer require php-di/php-di`
+ */
+$container = new \DI\Container();
+AppFactory::setContainer($container);
+
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
+$app = AppFactory::create();
 
 $container = $app->getContainer();
 
 // Logger
-$container['logger'] = function ($c) {
-
+$container->set('logger', function (\Psr\Container\ContainerInterface $container) {
     $logger = new Logger('activity');
     $logger->pushHandler(
         new StreamHandler('php://stdout', Logger::INFO)
     );
     return $logger;
-};
+});
 
 // MySQL database
-$container['mysql'] = function ($c) {
-    $logger = $c->get('logger');
+$container->set('mysql', function (\Psr\Container\ContainerInterface $container) {
+    $logger = $container->get('logger');
     $dsn = sprintf('mysql:dbname=%s;host=%s', getenv('MYSQL_DATABASE'), getenv('MYSQL_HOST'));
     $user = getenv('MYSQL_USER');
     $password = getenv('MYSQL_PASSWORD');
@@ -36,4 +51,23 @@ $container['mysql'] = function ($c) {
         }
     }
     throw new Exception(sprintf('Unable to connect to MySQL after %d attempts', $i));
-};
+});
+
+/**
+  * The routing middleware should be added earlier than the ErrorMiddleware
+  * Otherwise exceptions thrown from it will not be handled by the middleware
+  */
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Middleware
+ *
+ * @param bool                  $displayErrorDetails -> Should be set to false in production
+ * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool                  $logErrorDetails -> Display error details in error log
+ * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
+ *
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true, $container->get('logger'));
